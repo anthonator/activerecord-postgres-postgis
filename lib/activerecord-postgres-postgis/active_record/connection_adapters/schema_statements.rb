5685f3c6ae6b3faa27ada21f88b6834bd1726ee6
Anthony Smith <anthony@sticksnleaves.com>
2014-06-29 13:45:55 -0400

    Changed type option for add_geometry_column to spatial_type.

--- lib/activerecord-postgres-postgis/active_record/connection_adapters/schema_statements.rb
+++ lib/activerecord-postgres-postgis/active_record/connection_adapters/schema_statements.rb
@@ -20,10 +20,10 @@ module ActiveRecord
       # This method exists for backwards compatability with PostGIS 1.x.
       #
       # Example:
-      #   add_geometry_column :roads, :geom, srid: 423, type: 'LINESTRING', dimension: 2
+      #   add_geometry_column :roads, :geom, srid: 423, spatial_type: 'LINESTRING', dimension: 2
       #
       def add_geometry_column(table_name, column_name, options = {})
-        execute "SELECT AddGeometryColumn('#{table_name}', '#{column_name}', '#{options[:srid]}', '#{options[:type]}', '#{options[:dimension]}')"
+        execute "SELECT AddGeometryColumn('#{table_name}', '#{column_name}', '#{options[:srid]}', '#{options[:spatial_type]}', '#{options[:dimension]}')"
       end
     end
   end
