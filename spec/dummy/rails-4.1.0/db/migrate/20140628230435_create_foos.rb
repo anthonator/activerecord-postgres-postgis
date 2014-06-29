139f87380a880b7c4a5321eeac4d05c84996f873
Anthony Smith <anthony@sticksnleaves.com>
2014-06-29 13:58:08 -0400

    Added array column to make sure we dont trample over the stock adapters array implementation.

--- spec/dummy/rails-4.1.0/db/migrate/20140628230435_create_foos.rb
+++ spec/dummy/rails-4.1.0/db/migrate/20140628230435_create_foos.rb
@@ -1,7 +1,8 @@
 class CreateFoos < ActiveRecord::Migration
   def change
     create_table :foos do |t|
-      t.geometry :bar, srid: 423, type: 'LINESTRING', dimension: 2
+      t.geometry :bar, spatial_type: 'LINESTRING', srid: 423, dimension: 2
+      t.string   :some_array, array: true
 
       t.timestamps
     end
