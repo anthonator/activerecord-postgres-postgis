0e55522c5ea28f60eef9414ee0b69becbfe3bb8e
Anthony Smith <anthony@sticksnleaves.com>
2014-06-29 04:53:22 -0400

    Building geometry SQL definition.

--- lib/activerecord-postgres-postgis/active_record/connection_adapters/schema_statements.rb
+++ lib/activerecord-postgres-postgis/active_record/connection_adapters/schema_statements.rb
@@ -1,8 +1,21 @@
 module ActiveRecord
   module ConnectionAdapters
+    class PostgreSQLAdapter::SchemaCreation < ActiveRecord::ConnectionAdapters::AbstractAdapter::SchemaCreation
+      def add_column_options_with_spatial!(sql, options)
+        column = options.fetch(:column) { return super }
+        if column.type == :geometry && column.srid && column.spatial_type
+          sql << "(#{column.spatial_type},#{column.srid})"
+        else
+          add_column_options_without_spatial!(sql, options)
+        end
+      end
+
+      alias_method_chain :add_column_options!, :spatial
+    end
+
     module SchemaStatements
-      # Adds a spatial column to the table using the PostGIS "AddGeometryColumn()"
-      # function.
+      # Adds a spatial column to the table using the PostGIS
+      # "AddGeometryColumn()" function.
       #
       # This method exists for backwards compatability with PostGIS 1.x.
       #
@@ -10,11 +23,7 @@ module ActiveRecord
       #   add_geometry_column :roads, :geom, srid: 423, type: 'LINESTRING', dimension: 2
       #
       def add_geometry_column(table_name, column_name, options = {})
-        query_params = "'#{table_name}', '#{column_name}', '#{options[:srid]}', '#{options[:type]}', '#{options[:dimension]}'"
-        if options[:schema] || options[:schema_name]
-          query_params = "'#{(options[:schema] || options[:schema_name])}'" << query_params
-        end
-        execute "SELECT AddGeometryColumn(#{query_params})"
+        execute "SELECT AddGeometryColumn('#{table_name}', '#{column_name}', '#{options[:srid]}', '#{options[:type]}', '#{options[:dimension]}')"
       end
     end
   end
