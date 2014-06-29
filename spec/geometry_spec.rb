047866d83909bb884bf3aa494ebb85f3ba218df0
Anthony Smith <anthony@sticksnleaves.com>
2014-06-29 17:24:21 -0400

    Added specs to test saving geometry values.

--- spec/geometry_spec.rb
+++ spec/geometry_spec.rb
@@ -13,4 +13,18 @@ RSpec.describe :geometry do
     foo = Foo.new(geometry_without_options: nil)
     expect(foo.geometry_without_options).to be_nil
   end
+
+  it 'should save WKT and cast to rgeo' do
+    foo = Foo.new(geometry_without_options: 'LINESTRING(0 0, 1 1)')
+    foo.save!
+    foo.reload
+    expect(foo.geometry_without_options).to be_kind_of(RGeo::Feature::Geometry)
+  end
+
+  it 'should save WKB and cast to rgeo' do
+    foo = Foo.new(geometry_without_options: '00200000020000000000000002000000000000000000000000000000003ff00000000000003ff0000000000000')
+    foo.save!
+    foo.reload
+    expect(foo.geometry_without_options).to be_kind_of(RGeo::Feature::Geometry)
+  end
 end
