1454a419d2d6a3fe858681a26088855a430c4258
Anthony Smith <anthony@sticksnleaves.com>
2014-06-29 13:41:35 -0400

    Extracting spatial_type and srid option values.

--- lib/activerecord-postgres-postgis/active_record/connection_adapters/column.rb
+++ lib/activerecord-postgres-postgis/active_record/connection_adapters/column.rb
@@ -1,13 +1,31 @@
 module ActiveRecord
   module ConnectionAdapters
     class PostgreSQLColumn < Column
-      attr_accessor :srid, :spatial_type, :dimension
+      attr_reader :spatial_type, :srid, :dimension
+
+      def initialize_with_spatial(name, default, oid_type, sql_type = nil, null = true)
+        initialize_without_spatial(name, default, oid_type, sql_type, null)
+
+        @spatial_type = extract_spatial_type(sql_type)
+        @srid = extract_srid(sql_type)
+      end
+
+      alias_method_chain :initialize, :spatial
 
       def simplified_type_with_spatial(field_type)
-        field_type == 'geometry' ? :geometry : simplified_type_without_spatial(field_type)
+        field_type =~ /^(?:geometry)/ ? :geometry : simplified_type_without_spatial(field_type)
       end
 
       alias_method_chain :simplified_type, :spatial
+
+      private
+      def extract_spatial_type(sql_type)
+        $2.upcase if sql_type =~ /^(geometry)\(([a-z]+)(,\d+)?\)/i
+      end
+
+      def extract_srid(sql_type)
+        $4.to_i if sql_type =~ /^(geometry)\(([a-z]+)(,(\d+))\)/i
+      end
     end
   end
 end
