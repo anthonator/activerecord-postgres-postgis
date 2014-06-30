5876540d735993c78f2965740d51646cec06ab9f
Anthony Smith <anthony@sticksnleaves.com>
2014-06-30 10:02:45 -0400

    Removed stray puts.

--- spec/dummy/rails-4.1.0/config/boot.rb
+++ spec/dummy/rails-4.1.0/config/boot.rb
@@ -1,6 +1,4 @@
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../../../../Gemfile', __FILE__)
 
-puts ENV['BUNDLE_GEMFILE']
-
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
